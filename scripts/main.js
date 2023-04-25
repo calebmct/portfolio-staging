15b54c3d165d20ba091008b000136ae0567e68b0
calebmct <jmctwigan@gmail.com>
2023-04-25 15:40:02 -0400

    addition of reel video
    Added Reel
    Moved "Portfolio" to the correct folder - it was a separate repository before this

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    var imgurl = 'images/CM-logo.gif'+'?a='+Math.random();
-    $("#logo img").attr('src',imgurl);
-    $("#logo img").attr('alt','Caleb McTwigan');
-    console.log(imgurl);
-}); // ready function end
